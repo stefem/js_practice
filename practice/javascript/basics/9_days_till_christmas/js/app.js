f09e236649a192e182fdeb850cf563d975323e91
Stef Emiljanowicz <stefemil@live.co.uk>
2017-05-16 14:36:42 +0100

    With 'analogue' solution :-)

--- practice/javascript/basics/9_days_till_christmas/js/app.js
+++ practice/javascript/basics/9_days_till_christmas/js/app.js
@@ -17,6 +17,17 @@ Date.prototype.getDOY = function() {
 var dayOfYear = new Date().getDOY();
 alert(dayOfYear);
 
+/*
+today=new Date();
+var cmas=new Date(today.getFullYear(), 11, 25);
+if (today.getMonth()==11 && today.getDate()>25) 
+{
+cmas.setFullYear(cmas.getFullYear()+1); 
+}  
+var one_day=1000*60*60*24;
+console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+" days left until Christmas!");
+*/
 
 
 
