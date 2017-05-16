058257f1bd7e1a217440252aa413fb91ab03f20f
Stef Emiljanowicz <stefemil@live.co.uk>
2017-05-16 10:12:20 +0100

    Scroll function finished

--- practice/javascript/basics/5_text_scroll/js/app.js
+++ practice/javascript/basics/5_text_scroll/js/app.js
@@ -1,20 +1,13 @@
-// function scroll (input){
-// 	var output = "It works! ";
-// 	for (i=0; i<=input.length;i++) {
-// 	console.log(output);
-// 	}
-// };
-
-
-// setInterval(function(){}, 200)
-// // setInterval(scroll ("w3resource"), 200);
-
-// scroll ("w3resource");
-
-
 var input = "w3resource";
-var lastChr = input.toString().split('').pop();
-input = input.slice(0, -1);
-input = lastChr + input;
-console.log(input);
+var scrollInterval = 100;
+var myVar = setInterval(advanceScroll, scrollInterval);
+
+function advanceScroll() {
+    var lastChr = input.toString().split('').pop();
+    input = input.slice(0, -1);
+    input = lastChr + input;
+    document.getElementById("scroller").innerHTML = input;
+}
+
+
 
