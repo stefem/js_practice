85033441908abfb4e4e60617099c6dad472d51fb
Stef Emiljanowicz <stefemil@live.co.uk>
2017-05-16 14:35:24 +0100

    prototype commit - days till christmas

--- practice/javascript/basics/9_days_till_christmas/js/app.js
+++ practice/javascript/basics/9_days_till_christmas/js/app.js
@@ -1,17 +1,3 @@
-// // 1. Work out number of days in year
-// var year = new Date().getFullYear();
-// var days;
-
-// x = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
-// (x) ? days = 366 : days = 365;
-
-// // 2. work out current day number
-
-
-
-// // 3. subtract second from first
-
-
 Date.prototype.isLeapYear = function() {
     var year = this.getFullYear();
     if((year & 3) != 0) return false;
